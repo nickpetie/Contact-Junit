5261443c60ce1f54bd0a74b3304ba8955d76a562
Nick Peterson <nickpetie@gmail.com>
2014-04-23 15:40:32 -0700

    added comments in test file

--- test/sortingTest.java
+++ test/sortingTest.java
@@ -10,10 +10,10 @@ public class sortingTest {
 	public void test1() {
 		//fail("Not yet implemented");
 		
-		int[] num = {89,73, 67};
-		int[] expected = {67, 73, 89};
-		sortingBad.selectionSortInt(num);
-		assertArrayEquals(expected,num);
+		int[] num = {89,73, 67};                // setup input array
+		int[] expected = {67, 73, 89};          // the expected output array after sorted
+		sortingBad.selectionSortInt(num);       // sends array num to method to be sorted
+		assertArrayEquals(expected,num);        // compares new sorted array num with expected array.
 		
 	}
 	
